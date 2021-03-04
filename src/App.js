8ff7cff61cd97c8b7841a2d91da97db661bf7789
FoolyTD <trcariaga@utexas.edu>
2021-03-04 15:13:08 -0700

    Using React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const imageSrc =
+    "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg";
+function WelcomeBack({name = "valued customer"}) {
+  return <p>Welcome back, {name}!</p>
+}
+function DisplayImage() {
+  return <img src={imageSrc} alt="yeet"/>
+}
+
+const handleClick = (e) => console.log(e.target);
+
+const button = <button onClick={handleClick}>Click Me!</button>
+
+/* function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <WelcomeBack name="Jordan" />
+    <WelcomeBack name="Jose" />
+    <DisplayImage />
+    {button}
+    </>
+      );
+} */
+function App() {
+  const [lazy, setLazy] = useState(true);
+  console.log("Are you lazy?:",lazy)
+  return (
+    <section>
+      <p>Are you lazy?</p>
+      <button onClick={() => {
+        setLazy(!lazy); 
+        console.log(lazy)
+        }}>{lazy ? "You LAZY" : "You Not Lazy"}</button>
+    </section>
+  )
 }
 
 export default App;
